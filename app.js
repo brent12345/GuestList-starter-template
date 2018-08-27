5a7908e801bd219454c73672ee6196d3d61ed7aa
brent.morine <brentmorine@gmail.com>
2018-08-27 15:42:10 -0300

    adding section 3 tutorials

--- app.js
+++ app.js
@@ -9,22 +9,38 @@ new Vue({
             eventDescription: "It's back! This years summer festival will be in the beautiful countryside featuring our best line up ever! Add your name to the guest list for exclusive offers:"
         },
         newNameText: '',
-        guestName: ['Brent', 'Chris', 'Sam'], 
-        formSubmitClass: "",
+        guestName: [], 
         appStyles: {
             marginTop: '25px'
 
-        }
+        },
+        eventCapacity: 25,
+        eventCapacityPercentage: 0
         
     },
     methods: {
         formSubmitted() {
-            console.log(this.newNameText)
-            if(this.newNameText.length > 0) {
+            //console.log(this.newNameText)
+            console.log('method')
+            if(this.newNameText.length > 0 && this.eventCapacityPercentage < 100 ) {
                 this.guestName.push(this.newNameText)
                 this.newNameText = ''
-                this.formSubmitClass = "submitted"
+        
+                this.eventCapacityPercentage = this.guestName.length / (this.eventCapacity / 100)
             }
+        },
+        methodCall() {
+            this.appStyles.marginTop = '50px'
+        },
+     
+    },
+    computed: {
+        sortNames() {
+            console.log('computed')
+            return this.guestName.sort ()
+        },
+        test () {
+            console.log('method text')
         }
     }
 });
